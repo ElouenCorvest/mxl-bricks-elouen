36a51d33a611b60ead3ef1438dc5c321cb66f612
Marvin van Aalst <marvin.vanaalst@gmail.com>
2025-06-23 07:16:09 +0200

    usr: new: use mxlpy 0.22

--- src/mxlbricks/utils.py
+++ src/mxlbricks/utils.py
@@ -37,14 +37,16 @@ def filter_stoichiometry(
     optional: dict[str, float] | None = None,
 ) -> Mapping[str, float | Derived]:
     """Only use components that are actually compounds in the model"""
+    variables = model.get_raw_variables(as_copy=False)
+
     new = {}
     for k, v in stoichiometry.items():
-        if k in model.variables:
+        if k in variables:
             new[k] = v
         elif k not in model._ids:  # noqa: SLF001
             msg = f"Missing component {k}"
             raise KeyError(msg)
 
     optional = {} if optional is None else optional
-    new |= {k: v for k, v in optional.items() if k in model.variables}
+    new |= {k: v for k, v in optional.items() if k in variables}
     return new
